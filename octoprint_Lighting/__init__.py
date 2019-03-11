a07da0bd02bb49d17da72e53b0cafcbdfaf456a6
kylebranham67 <kylebranham67@yahoo.com>
2019-03-11 15:00:07 -0400

    Update __init__.py

--- octoprint_Lighting/__init__.py
+++ octoprint_Lighting/__init__.py
@@ -1,21 +1,30 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
 class LightingPlugin(octoprint.plugin.SettingsPlugin,
                      octoprint.plugin.AssetPlugin,
-                     octoprint.plugin.TemplatePlugin):
+                     octoprint.plugin.TemplatePlugin
+		     octoprint.plugin.StartupPlugin):
 
 	##~~ SettingsPlugin mixin
+	def __init__(self):
+		self._checkTempTimer = None
+
+	def _restartTimer(self):
+		# stop the timer
+		if self._checkTempTimer:
+			self._logger.debug(u"Stopping Timer...")
+			self._checkTempTimer.cancel()
+			self._checkTempTimer = None
+
+		# start a new timer
+		interval = self._settings.get_int(['interval'])
+		if self._settings.get_boolean(['enabled']) and interval:
+			self._logger.debug(u"Starting Timer...")
+			self._checkTempTimer = RepeatedTimer(interval, self.CheckTemps, None, None, True)
+			self._checkTempTimer.start()
 
 	def get_settings_defaults(self):
 		return dict(
@@ -54,6 +63,55 @@ class LightingPlugin(octoprint.plugin.SettingsPlugin,
 				pip="https://github.com/kylebranham67/octoprint_Lighting/archive/{target_version}.zip"
 			)
 		)
+	
+	def CheckTemps(self):
+		temps = self._printer.get_current_temperatures()
+		self._logger.debug(u"CheckTemps(): %r" % (temps,))
+		if not temps:
+			self._logger.debug(u"No Temperature Data")
+			return
+
+		for k in temps.keys():
+			# example dictionary from octoprint
+			# {
+			#   'bed': {'actual': 0.9, 'target': 0.0, 'offset': 0},
+			#   'tool0': {'actual': 0.0, 'target': 0.0, 'offset': 0},
+			#   'tool1': {'actual': 0.0, 'target': 0.0, 'offset': 0}
+			# }
+			if k == 'bed':
+				threshold_high = self._settings.get_int(['bed'])
+				threshold_low = self._settings.get_int(['bed_low'])
+			else:
+				threshold_high = self._settings.get_int(['hotend'])
+				threshold_low = self._settings.get_int(['hotend_low'])
+
+			violation = False
+			errmsg = u"TemperatureFailSafe violation, heater: {heater}: {temp}C {exp} {threshold}C"
+			if threshold_high and temps[k]['actual'] > threshold_high:
+				errmsg = errmsg.format(heater=k, temp=temps[k]['actual'], exp=">", threshold=threshold_high)
+				violation = True
+
+			# only check the low thresholds if we are currently printing, or else ignore it
+			if self._printer.is_printing() and threshold_low and temps[k]['actual'] < threshold_low:
+				errmsg = errmsg.format(heater=k, temp=temps[k]['actual'], exp="<", threshold=threshold_low)
+				violation = True
+
+			if violation:
+				# alert the user
+				self._logger.error(errmsg)
+				self._plugin_manager.send_plugin_message(__plugin_name__, dict(type="popup", msg=errmsg))
+
+				env = {}
+				env["TEMPERATURE_FAILSAFE_FAULT_TOOL"] = str(k)
+				env["TEMPERATURE_FAILSAFE_FAULT_HIGH_THRESHOLD"] = str(threshold_high)
+				env["TEMPERATURE_FAILSAFE_FAULT_LOW_THRESHOLD"] = str(threshold_low)
+
+				# place the temperatures into an environment dictionary to pass to the remote program
+				for t in temps.keys():
+					env["TEMPERATURE_FAILSAFE_%s_ACTUAL" % t.upper()] = str(temps[t]['actual'])
+					env["TEMPERATURE_FAILSAFE_%s_TARGET" % t.upper()] = str(temps[t]['target'])
+
+				#self._executeFailsafe(env)
 
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
